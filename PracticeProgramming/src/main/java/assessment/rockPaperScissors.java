8e7643505e5865e716c7cd84b9bcf0729e374f33
AlinaLauraV <vizitiu.alinalaura@yahoo.it>
2022-10-14 16:08:13 +0100

    Assessment: Basic Programming Concepts - second

--- PracticeProgramming/src/main/java/assessment/rockPaperScissors.java
+++ PracticeProgramming/src/main/java/assessment/rockPaperScissors.java
@@ -15,25 +15,37 @@ public class rockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How many rounds of Rock-Paper-Scissors would you like to play?");
+        System.out.println("How many rounds of Rock-Paper-Scissors would you like to play? Pease choose between 1 and 10!");
         int rounds = Integer.parseInt(scanner.nextLine());
         
         for (int i = 0; i < rounds; i++) { 
             if (rounds < 0){
-            System.out.println("Error!");
+            System.out.println("Error!You have to choose between 1 and 10");
             }
             else if (rounds > 10){
-            System.out.println("Error!");
+            System.out.println("Error!You have to choose between 1 and 10");
             }
             else {
                 playRockPaperSc(scanner);
                 }
             }
-    }
+    
+        System.out.println("Do you want to play again? Type yes or no");
+        Scanner userInput = new Scanner(System.in);
+        String newRound = userInput.nextLine();
+        if (newRound.equals("yes")){
+            playRockPaperSc(scanner);
+                    }
+        else{
+           System.out.println("Thanks for playing!");
+        }
+        
+}
+
     
     static void playRockPaperSc(Scanner scanner) {
         // user input
-        System.out.println("Choose please! (rock/paper/scissors)");
+        System.out.println("Choose please! (1= rock/2 = paper/3 = scissors)");
         String playerMove = scanner.nextLine();
 
         // computer input
@@ -56,11 +68,11 @@ public class rockPaperScissors {
 
         // results
         if (playerMove.equals(computerMove)) {
-            tie ++;
+            tie =tie+1;
             System.out.println("It's a tie!");
             System.out.println("Final results: " + "ties: " + tie + " user wins: " + userWin + " computer wins: " + computerWin);
         } else if (playerWins(playerMove, computerMove)) {
-            userWin ++;
+            userWin = userWin+1;
             System.out.println("You won!");
             System.out.println("Final results: " + "ties: " + tie + " user wins: " + userWin + " computer wins: " + computerWin);
         } else {
